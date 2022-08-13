865ad034d79d9eb9b94080d7485e5a3f9ee4cae0
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-08-13 09:45:06 +0300

    practising writing of methods in Ruby

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default name ="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add num1 , num2
+    num1 + num2;
+end
+
+add(5,6)
+
+def halve number
+    return nil if number.class != Integer
+    number/2
+    end
+halve(6)
